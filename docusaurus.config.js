f1039aeb41b5ee46825d83685f3633bc989bb881
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-08-26 19:08:50 +0200

    Configure

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,18 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: 'My Site',
-    tagline: 'Dinosaurs are cool',
+    title: 'Sqowey - Docs',
+    tagline: 'Documentation for Sqowey',
     url: 'https://your-docusaurus-test-site.com',
     baseUrl: '/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon.ico',
+    favicon: 'img/favicon_transparent.ico',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'sqowey', // Usually your GitHub org/user name.
+    projectName: 'docs', // Usually your repo name.
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
@@ -55,49 +55,41 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
         navbar: {
-            title: 'My Site',
+            title: 'Sqowey',
             logo: {
-                alt: 'My Site Logo',
-                src: 'img/logo.svg',
+                alt: 'Sqowey Logo',
+                src: 'img/logo_transparent.png',
             },
-            items: [{
-                    type: 'doc',
-                    docId: 'intro',
-                    position: 'left',
-                    label: 'Tutorial',
-                },
-                { to: '/blog', label: 'Blog', position: 'left' },
-                {
-                    href: 'https://github.com/facebook/docusaurus',
-                    label: 'GitHub',
-                    position: 'right',
-                },
+            items: [
+                // {
+                //     type: 'doc',
+                //     docId: 'intro',
+                //     position: 'left',
+                //     label: 'Tutorial',
+                // },
+                // { to: '/blog', label: 'Blog', position: 'left' },
+                // {
+                //     href: 'https://github.com/facebook/docusaurus',
+                //     label: 'GitHub',
+                //     position: 'right',
+                // },
             ],
         },
         footer: {
             style: 'dark',
             links: [{
-                    title: 'Docs',
+                    title: 'Documentation',
                     items: [{
-                        label: 'Tutorial',
+                        label: 'Introduction',
                         to: '/docs/intro',
                     }, ],
                 },
                 {
                     title: 'Community',
                     items: [{
-                            label: 'Stack Overflow',
-                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                        },
-                        {
-                            label: 'Discord',
-                            href: 'https://discordapp.com/invite/docusaurus',
-                        },
-                        {
-                            label: 'Twitter',
-                            href: 'https://twitter.com/docusaurus',
-                        },
-                    ],
+                        label: 'Twitter',
+                        href: 'https://twitter.com/sqowey',
+                    }, ],
                 },
                 {
                     title: 'More',
@@ -112,7 +104,7 @@ const config = {
                     ],
                 },
             ],
-            copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+            copyright: `Copyright © ${new Date().getFullYear()} Sqowey.<br>Built with Docusaurus.`,
         },
         prism: {
             theme: lightCodeTheme,
